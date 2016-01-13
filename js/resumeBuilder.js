4bb9c5261adc71d56ea3b2c5e22d1dec380d3d0b
rafaeljgomezg <rgomezshares@hotmail.com>
2016-01-13 15:28:48 -0430

    HTMLheaderName and HTMLheaderLog updated on resumebuilder.js Hello World erased

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,37 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //$("#main").append("Rafael Gomez");
+
+
+ var name = "Rafael Gomez";
+ var role = "System Engineer";
+
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+//$("#header").append(formattedName);
+//$("#header").append(formattedRole);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+// ************Esto es una prueba especial****************
+var s="audacity"
+
+var udacityizer = function(s) {  
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+    
+    s = s[1].toUpperCase()+s.slice(2);
+    return s;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s)); 
+
+// *****************************************
